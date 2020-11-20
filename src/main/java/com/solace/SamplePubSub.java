bccb6a1927190832e530465cf7c49671d5445afe
Julian Setiawan <julian.setiawan@solace.com>
2020-11-20 10:07:31 -0500

    Add sample pubsub

---
+++ src/main/java/com/solace/SamplePubSub.java
@@ -0,0 +1,44 @@
+package com.solace;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class SamplePubSub implements MqttCallback {
+    public static void main(String[] args) throws Exception {
+        String url = args[0];
+        String username = args[1];
+        String password = args[2];
+
+        String topicBase = "iot/" + username;
+
+        MqttClient client = new MqttClient(url, username);
+        client.setCallback(new SamplePubSub());
+
+        MqttConnectOptions connectOptions = new MqttConnectOptions();
+        connectOptions.setUserName(username);
+        connectOptions.setPassword(password.toCharArray());
+
+        client.connect(connectOptions);
+
+        client.subscribe(topicBase + "/messages");
+        client.publish(topicBase + "/heartbeats", new MqttMessage("ping".getBytes()));
+    }
+
+    @Override
+    public void connectionLost(Throwable throwable) {
+
+    }
+
+    @Override
+    public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+        System.out.println("Received message on topic " + topic + ": " + mqttMessage);
+    }
+
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+    }
+}
